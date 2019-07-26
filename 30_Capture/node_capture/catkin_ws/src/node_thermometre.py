b36545987436d351c0eaffde4a9467d5e9582165
adrienbeaudrey <adrien.beaudrey@gmail.com>
2019-07-26 21:21:44 +0200

    Ad

--- 30_Capture/node_capture/catkin_ws/src/node_thermometre.py
+++ 30_Capture/node_capture/catkin_ws/src/node_thermometre.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 import rospy
+import sys
+import Adafruit_DHT
 from std_msgs.msg import String
 
 def thermometre():
     pub = rospy.Publisher('temperature', String, queue_size=10)
     rospy.init_node('capture_temperature', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+
     while not rospy.is_shutdown():
-        output_str = "La température est de: %s" % rospy.get_time()
+        humidity, temperature = Adafruit_DHT.read_retry(11,4)
+
+        output_str = 'Temp: {0:0.1f} C Humidity: {1:0.1f} %'.format(temperature, humidity)
+#        output_str = "La température est de: %s" % rospy.get_time()
         rospy.loginfo(ouput_str)
         pub.publish(output_str)
         rate.sleep()
