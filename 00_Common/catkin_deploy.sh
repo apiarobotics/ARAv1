8f694658f900deabe52642ce1543ced3fcccbff2
adrienbeaudrey <adrien.beaudrey@gmail.com>
2019-07-27 17:04:22 +0200

    Ad

--- 00_Common/catkin_deploy.sh
+++ 00_Common/catkin_deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-read -p "Enter pkg name: " pkg_name
+export ROS_MASTER_URI="http://172.18.0.2:11311/"
+
+#read -p "Enter pkg name: " pkg_name
 cd ~/catkin_ws/src
-catkin_create_pkg pkg_name std_mgsg rospy
-cd ../
-catkin_make
-source devel/setup.bash
+catkin_create_pkg $NODE_NAME std_msgs rospy
+cd ~/catkin_ws
+#catkin_make
+/bin/bash ~/catkin_ws/devel/setup.bash
+
+mv ~/catkin_ws/src/*.py ~/catkin_ws/src/$NODE_NAME/src/
--- 10_Computer/node_p/5_run.sh
+++ 10_Computer/node_p/5_run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#sudo docker run -it --rm --net ara --name node_p arav1/node_p:0.1 bash
+sudo docker run -it --rm --net ara -e NODE_NAME=node_p -w /root --name node_p arav1/node_p:0.1 /bin/bash 
 
-sudo docker run -it --rm --net ara --name node_p -v /~/ARAv1/00_Common:/root/ara_run_scripts/: arav1/node_p:0.1 bash
