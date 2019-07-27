65af35d5b76d6eeb01b51665e5f0126c5b00a3f5
adrienbeaudrey <adrien.beaudrey@gmail.com>
2019-07-27 12:29:53 +0200

    2019.07.27:Ad

--- 10_Computer/node_p/5_run.sh
+++ 10_Computer/node_p/5_run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo docker run -it --rm --net ara --name node_p arav1/node_p:0.1 bash
+#sudo docker run -it --rm --net ara --name node_p arav1/node_p:0.1 bash
+
+sudo docker run -it --rm --net ara --name node_p -v /~/ARAv1/00_Common:/root/ara_run_scripts/: arav1/node_p:0.1 bash
