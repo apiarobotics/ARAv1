3d9c67c91398a4cf8f9cb12055a4aebde926ad15
adrienbeaudrey <adrien.beaudrey@gmail.com>
2019-08-05 03:40:28 +0200

    ad

--- 00_Global/0g_install.sh
+++ 00_Global/0g_install.sh
@@ -56,7 +56,7 @@ pushNetwork (){
 			
 			sudo echo "$VAR_NAME $VAR_HOST" >> /etc/hosts
 			
-			sudo ssh-copy-id -i ~/.ssh/master.pub ubuntu@$VAR_IP
+			sudo ssh-copy-id -i ~/.ssh/master.key.pub ubuntu@$VAR_IP
 					
         done < "$NET_PATH"
 	
